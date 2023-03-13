4ab437ff78ce008a9cdcf3fa28e5164eb5ce4563
tochyodikwa <odikwatochy233@gmail.com>
2023-03-13 04:44:47 -0700

    First

---
+++ libraryfancycoverflow/build/generated/source/buildConfig/debug/at/technikum/mti/fancycoverflow/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package at.technikum.mti.fancycoverflow;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String LIBRARY_PACKAGE_NAME = "at.technikum.mti.fancycoverflow";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
